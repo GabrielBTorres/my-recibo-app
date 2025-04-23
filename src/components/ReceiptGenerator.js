ce8561b60e3b102ff4e4b97e5475a16255679c1e
GabrielBTorres <111030829+GabrielBTorres@users.noreply.github.com>
2025-04-22 22:22:05 -0300

    modo vertical

--- src/components/ReceiptGenerator.js
+++ src/components/ReceiptGenerator.js
@@ -58,7 +58,7 @@ export default function ReceiptGenerator() {
       windowHeight: element.scrollHeight,
     }).then(canvas => {
       const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('landscape', 'pt', 'a4');
+      const pdf = new jsPDF('p', 'pt', 'a4');
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
       pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
